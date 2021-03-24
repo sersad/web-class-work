0464d3dc3a68a4d85ae2989fdda373f8dbb070a9
Sergey Sadovnikov <sergey.sadovnikov@gmail.com>
2021-03-24 18:27:35 +0300

    web class work

--- main.py
+++ main.py
@@ -277,7 +277,6 @@ def main():
     # app.run(port="8080", host="127.0.0.1")
 
     port = int(os.environ.get('PORT', 5000))
-
     # с дефаултными значениями будет не более 4 потов
     serve(app, port=port, host="0.0.0.0")
 
